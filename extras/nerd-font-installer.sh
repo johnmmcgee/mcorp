f15070739ecec76fe8440644787aa234c8b38cf3
John M McGee <john.m.mcgee57.ctr@army.mil>
2025-03-10 16:31:05 -0400

    slim down script

--- extras/nerd-font-installer.sh
+++ extras/nerd-font-installer.sh
@@ -27,48 +27,13 @@ download_and_install_font() {
     echo "[-] ${font_name} Nerd Font installed successfully! [-]"
 }
 
-fons_list=("3270" "Agave" "AnonymousPro" "Arimo" "AurulentSansMono" "BigBlueTerminal" "BitstreamVeraSansMono" "CascadiaCode" "CodeNewRoman" "ComicShannsMono" "Cousine" "DaddyTimeMono" "DejaVuSansMono" "DroidSansMono" "FantasqueSansMono" "FiraCode" "FiraMono" "Gohu" "Go-Mono" "Hack" "Hasklig" "HeavyData" "Hermit" "iA-Writer" "IBMPlexMono" "InconsolataGo" "InconsolataLGC" "Inconsolata" "IosevkaTerm" "Iosevka" "JetBrainsMono" "Lekton" "LiberationMono" "Lilex" "Meslo" "Monofur" "Monoid" "Mononoki" "MPlus" "NerdFontsSymbolsOnly" "Noto" "OpenDyslexic" "Overpass" "ProFont" "ProggyClean" "RobotoMono" "ShareTechMono" "SourceCodePro" "SpaceMono" "Terminus" "Tinos" "UbuntuMono" "Ubuntu" "VictorMono")
-
 if [ "$#" -gt 0 ]; then
     for font_name in "$@"; do
         echo "[-] Downloading ${font_name} Nerd Font [-]"
         download_and_install_font "$font_name"
     done
-else
-    if [ -t 0 ]; then  # Check if script has a terminal
-        # Print available fonts in multi-column format
-        echo "Available Fonts:"
-        for i in "${!fons_list[@]}"; do 
-            printf "%3d: %-20s" "$i" "${fons_list[$i]}"
-            if (( ($i + 1) % 3 == 0 )); then
-                echo
-            fi
-        done
-        echo
-
-        # Ask user to select fonts
-        echo "Enter the numbers of the fonts you want to install, separated by space (e.g., 1 3 5):"
-        read -a selected_indexes
-
-        # Confirm selection
-        echo "You've selected:"
-        for i in "${selected_indexes[@]}"; do
-            echo "$i: ${fons_list[$i]}"
-        done
-
-        echo "Proceed with installation? [y/N]"
-        read proceed
-
-        if [[ "$proceed" == "Y" || "$proceed" == "y" ]]; then
-            for i in "${selected_indexes[@]}"; do
-                echo "[-] Downloading ${fons_list[$i]} Nerd Font [-]"
-                download_and_install_font "${fons_list[$i]}"
-            done
-        else
-            echo "Installation aborted."
-        fi
     else
-        echo "No arguments provided and no terminal available for interactive selection. Aborting."
+        echo "No arguments provided. Aborting."
         exit 1
     fi
 fi
